374f56e8c923409c09f76fd1f2cc1c7b265f1747
Mohammed Dawood <mahmedtony73@gmail.com>
2022-09-22 16:10:04 +0200

    add Imperative or programmatic navigation

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,18 +1,24 @@
-import { Link } from 'react-router-dom';
+// import { useEffect } from "react";
+import { Link } from "react-router-dom"; //, useNavigate
 
 const Products = () => {
+  // programatic navigation or Impairative navigation
+  // const navigate = useNavigate();
+  // useEffect(() => {
+  //   navigate("/products/p1");
+  // }, [navigate]);
   return (
     <section>
       <h1>The Products Page</h1>
       <ul>
         <li>
-          <Link to='/products/p1'>A Book</Link>
+          <Link to="/products/p1">A Book</Link>
         </li>
         <li>
-          <Link to='/products/p2'>A Carpet</Link>
+          <Link to="/products/p2">A Carpet</Link>
         </li>
         <li>
-          <Link to='/products/p3'>An Online Course</Link>
+          <Link to="/products/p3">An Online Course</Link>
         </li>
       </ul>
     </section>
